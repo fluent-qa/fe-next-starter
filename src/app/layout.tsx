5a66197a6d8e7ff1c8e99f77b10ce871344a8e45
DamnPat <patrickwuke@163.com>
2025-01-02 15:22:57 +0800

    setup sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   const defaultOpen=false
+  //todo: add  checking
   return (
     <html lang="en" suppressHydrationWarning>
     <body className={cn(
@@ -26,7 +27,7 @@ export default function RootLayout({
     )}>
       <ThemeProvider
         attribute="class"
-        defaultTheme="system"
+        defaultTheme="light"
         enableSystem
         disableTransitionOnChange
       >
